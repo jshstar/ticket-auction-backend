4dca074b4fa915e0a61718f373ab30ab4db8b069
Kimhyeyun <cqqwer@gmail.com>
2024-01-06 23:08:07 +0900

    #22 - [init - user] 회원 entity에 @Comment 애노테이션으로 description 추가

--- src/main/java/com/sparta/ticketauction/domain/user/entity/User.java
+++ src/main/java/com/sparta/ticketauction/domain/user/entity/User.java
@@ -1,6 +1,7 @@
 package com.sparta.ticketauction.domain.user.entity;
 
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Comment;
 
 import com.sparta.ticketauction.domain.user.entity.constant.Role;
 
@@ -21,24 +22,31 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Comment("회원 이메일")
 	@Column(name = "email")
 	private String email;
 
+	@Comment("회원 비밀번호")
 	@Column(name = "password")
 	private String password;
 
+	@Comment("회원 이름")
 	@Column(name = "name")
 	private String name;
 
+	@Comment("회원 닉네임")
 	@Column(name = "nickname")
 	private String nickname;
 
+	@Comment("회원 전화번호")
 	@Column(name = "phone_number")
 	private String phoneNumber;
 
+	@Comment("회원 역할(관리자 or 일반 유저)")
 	@Column(name = "role")
 	private Role role = Role.USER;
 
+	@Comment("회원 보유 포인트")
 	@Column(name = "point")
 	@ColumnDefault("0")
 	private long point;
