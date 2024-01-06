e166a71d0fcb72210d4256c6a5361965af61730a
Kimhyeyun <cqqwer@gmail.com>
2024-01-06 20:52:22 +0900

    #22- [init - user] 회원 엔티티 내의 컬럼 추가

--- src/main/java/com/sparta/ticketauction/domain/user/entity/User.java
+++ src/main/java/com/sparta/ticketauction/domain/user/entity/User.java
@@ -1,15 +1,45 @@
 package com.sparta.ticketauction.domain.user.entity;
 
+import org.hibernate.annotations.ColumnDefault;
+
 import com.sparta.ticketauction.domain.user.entity.constant.Role;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
 
+@Entity
+@Getter
+@Table(name = "users")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(name = "email")
 	private String email;
+
+	@Column(name = "password")
 	private String password;
+
+	@Column(name = "name")
 	private String name;
+
+	@Column(name = "nickname")
 	private String nickname;
+
+	@Column(name = "phone_number")
 	private String phoneNumber;
-	private Role role;
+
+	@Column(name = "role")
+	private Role role = Role.USER;
+
+	@Column(name = "point")
+	@ColumnDefault("0")
+	private long point;
 }
