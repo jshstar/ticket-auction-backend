7759acc6affee66dd54e082a5755ab12000865ab
Kimhyeyun <cqqwer@gmail.com>
2024-01-06 15:44:12 +0900

    #25- [init - exception] 공통으로 사용할 exception handler 및 exception class 구현

---
+++ src/main/java/com/sparta/ticketauction/global/exception/ErrorCode.java
@@ -0,0 +1,69 @@
+package com.sparta.ticketauction.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+	/* USER */
+
+
+
+
+
+
+
+
+
+
+	/* GOODS */
+
+
+
+
+
+
+
+
+
+	/* PLACE */
+
+
+
+
+
+
+
+
+
+	/* AUCTION */
+
+
+
+
+
+
+
+
+
+	/* RESERVATION */
+
+
+
+
+
+
+
+
+
+	/* GLOBAL */
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "", "");
+
+	private HttpStatus httpStatus;
+	private String code;
+	private String message;
+}
