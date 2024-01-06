a8cfc9f96ee6ed19cd4fc0c03e18e1a204182f38
Kimhyeyun <cqqwer@gmail.com>
2024-01-07 03:00:06 +0900

    #22 - [fix-user] request에서 생년월일 입력하는 거 추가함

--- src/main/java/com/sparta/ticketauction/domain/user/request/UserCreateRequest.java
+++ src/main/java/com/sparta/ticketauction/domain/user/request/UserCreateRequest.java
@@ -1,5 +1,9 @@
 package com.sparta.ticketauction.domain.user.request;
 
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -33,4 +37,8 @@ public class UserCreateRequest {
 	@NotBlank(message = "필수 입력입니다.")
 	@Pattern(regexp = "^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}$", message = "전화번호 형식으로 입력해주세요.")
 	private final String phoneNumber;
+
+	@NotBlank(message = "필수 입력입니다.")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private final LocalDate birth;
 }
