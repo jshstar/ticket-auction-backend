d01c10bb8f0dfc70d5e83a3bb7a0895d34638412
Kimhyeyun <cqqwer@gmail.com>
2024-01-08 01:38:25 +0900

    #15 [init - login] UserDetails와 UserDetailsService 구현체 구현

--- src/main/java/com/sparta/ticketauction/domain/user/repository/UserRepository.java
+++ src/main/java/com/sparta/ticketauction/domain/user/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.sparta.ticketauction.domain.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sparta.ticketauction.domain.user.entity.User;
@@ -9,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	Boolean existsByEmail(String email);
 
 	Boolean existsByNickname(String nickname);
+
+	Optional<User> findByNickname(String nickname);
 }
---
+++ src/main/java/com/sparta/ticketauction/domain/user/security/UserDetailServiceImpl.java
@@ -0,0 +1,30 @@
+package com.sparta.ticketauction.domain.user.security;
+
+import static com.sparta.ticketauction.global.exception.ErrorCode.*;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.sparta.ticketauction.domain.user.entity.User;
+import com.sparta.ticketauction.domain.user.repository.UserRepository;
+import com.sparta.ticketauction.global.exception.ApiException;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByNickname(username).orElseThrow(
+			() -> new ApiException(NOT_FOUND_USER)
+		);
+
+		return new UserDetailsImpl(user);
+	}
+}
