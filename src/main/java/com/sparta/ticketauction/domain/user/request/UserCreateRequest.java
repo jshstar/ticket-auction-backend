408e0218c6ae64585f59b3b05a78dcc20340870c
Kimhyeyun <cqqwer@gmail.com>
2024-01-07 21:53:20 +0900

    #1 [fix - user] 비밀번호 입력 정규식 수정

--- src/main/java/com/sparta/ticketauction/domain/user/request/UserCreateRequest.java
+++ src/main/java/com/sparta/ticketauction/domain/user/request/UserCreateRequest.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -20,7 +21,7 @@ public class UserCreateRequest {
 	private final String email;
 
 	@Pattern(
-		regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^])(?=.*[0-9]).$",
+		regexp = "^(?=.*?[a-zA-Z])(?=.*?[0-9])(?=.*?[#!@$ %^])[a-zA-Z0-9@#$%^!]*$",
 		message = "영어, 숫자, 특수문자(!,@,#,$,%,^) 조합으로 입력해주세요."
 	)
 	@Size(min = 8, max = 15, message = "최소 8자, 최대 15자로 입력해주세요.")
@@ -38,7 +39,7 @@ public class UserCreateRequest {
 	@Pattern(regexp = "^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}$", message = "전화번호 형식으로 입력해주세요.")
 	private final String phoneNumber;
 
-	@NotBlank(message = "필수 입력입니다.")
+	@NotNull(message = "필수 입력입니다.")
 	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
 	private final LocalDate birth;
 }
