8a8a7d628c67d33f09d62a889807b06a5e9a0b6e
MinJung Kim <kminss.dev@gmail.com>
2024-01-07 18:02:46 +0900

    #23 [fix-auction] 경매 도메인 구현 - 입찰 엔티티 수정
      - ManyToOne fetch 타입 수정
        기본 EAGER 에서 LAZY 로 수정

--- src/main/java/com/sparta/ticketauction/domain/auction/entity/Bid.java
+++ src/main/java/com/sparta/ticketauction/domain/auction/entity/Bid.java
@@ -6,6 +6,7 @@ import com.sparta.ticketauction.domain.user.entity.User;
 import com.sparta.ticketauction.global.entity.BaseEntity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -28,12 +29,12 @@ public class Bid extends BaseEntity {
 	private Long id;
 
 	@Comment("입찰한 유저")
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
 	private User user;
 
 	@Comment("경매")
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "auction_id")
 	private Auction auction;
 
