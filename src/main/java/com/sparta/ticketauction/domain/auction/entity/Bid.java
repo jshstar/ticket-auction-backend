c3fcdef745d2529219dc8b89f34e99ee3e11c6ac
MinJung Kim <kminss.dev@gmail.com>
2024-01-07 16:54:12 +0900

    #23 [init-auction] 경매 도메인 구현 - 입찰 엔티티 구현
    - 입찰(Bid.java) 엔티티 구현

--- src/main/java/com/sparta/ticketauction/domain/auction/entity/Bid.java
+++ src/main/java/com/sparta/ticketauction/domain/auction/entity/Bid.java
@@ -1,4 +1,48 @@
 package com.sparta.ticketauction.domain.auction.entity;
 
-public class Bid {
+import org.hibernate.annotations.Comment;
+
+import com.sparta.ticketauction.domain.user.entity.User;
+import com.sparta.ticketauction.global.entity.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "auction")
+@Entity
+public class Bid extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Comment("입찰한 유저")
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Comment("경매")
+	@ManyToOne
+	@JoinColumn(name = "auction_id")
+	private Auction auction;
+
+	private Bid(User user, Auction auction) {
+		this.user = user;
+		this.auction = auction;
+	}
+
+	public static Bid of(User user, Auction auction) {
+		return new Bid(user, auction);
+	}
 }
