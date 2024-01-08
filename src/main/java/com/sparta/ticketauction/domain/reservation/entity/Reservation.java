b7c4894dc4ada79f455415b17ceff421b8e1c6ec
OuOHoon <begreat@kakao.com>
2024-01-08 22:59:35 +0900

    #19 [refactor-reservation] Reservation Entity의 생성자 Builder 추가 및 Column nullable=false 지정

--- src/main/java/com/sparta/ticketauction/domain/reservation/entity/Reservation.java
+++ src/main/java/com/sparta/ticketauction/domain/reservation/entity/Reservation.java
@@ -19,6 +19,7 @@ import jakarta.persistence.JoinColumns;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -30,38 +31,35 @@ public class Reservation extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false)
 	private Long id;
 
-	@Comment("공연 회차별 좌석")
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumns({
-		@JoinColumn(name = "seat_id", referencedColumnName = "seat_id"),
-		@JoinColumn(name = "sequence_id", referencedColumnName = "sequence_id")
+		@JoinColumn(name = "seat_id", referencedColumnName = "seat_id", nullable = false),
+		@JoinColumn(name = "sequence_id", referencedColumnName = "sequence_id", nullable = false)
 	})
 	private GoodsSequenceSeat goodsSequenceSeat;
 
 	@Comment("유저")
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id")
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 
 	@Comment("예매 가격")
-	@Column(name = "price")
+	@Column(name = "price", nullable = false)
 	private Long price;
 
 	@Comment("예매 상태")
-	@Column(name = "status", length = 10)
+	@Column(name = "status", length = 10, nullable = false)
 	@Enumerated(EnumType.STRING)
 	private ReservationStatus status;
 
+	@Builder
 	private Reservation(User user, GoodsSequenceSeat goodsSequenceSeat, Long price) {
 		this.user = user;
 		this.goodsSequenceSeat = goodsSequenceSeat;
 		this.price = price;
 		this.status = ReservationStatus.OK;
-	}
-
-	public static Reservation of(User user, GoodsSequenceSeat goodsSequenceSeat, Long price) {
-		return new Reservation(user, goodsSequenceSeat, price);
 	}
 }
