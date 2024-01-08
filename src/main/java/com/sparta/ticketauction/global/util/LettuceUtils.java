5e49652e5312edfa64ab067c2f4374e3852fd95a
Kimhyeyun <cqqwer@gmail.com>
2024-01-08 19:45:21 +0900

    #15 [init - user] 유저의 리프레시 토큰 저장을 위해 Lettuce 설정 파일과 Util 파일 구현

---
+++ src/main/java/com/sparta/ticketauction/global/util/LettuceUtils.java
@@ -0,0 +1,32 @@
+package com.sparta.ticketauction.global.util;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class LettuceUtils {
+
+	public static final int REFRESH_TOKEN_EXPIRATION = 60 * 60 * 24 * 14;
+	private final RedisTemplate<String, String> lettuceTemplate;
+
+	public void save(String key, String value) {
+		lettuceTemplate.opsForValue().set(key, value, REFRESH_TOKEN_EXPIRATION, TimeUnit.SECONDS);
+	}
+
+	public String get(String key) {
+		return lettuceTemplate.opsForValue().get(key);
+	}
+
+	public boolean delete(String key) {
+		return Boolean.TRUE.equals(lettuceTemplate.delete(key));
+	}
+
+	public boolean hasKey(String key) {
+		return Boolean.TRUE.equals(lettuceTemplate.hasKey(key));
+	}
+}
