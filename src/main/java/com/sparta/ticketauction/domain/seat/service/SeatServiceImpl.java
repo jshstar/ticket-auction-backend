4deebac068ee728dca404c909bda5c4fa62bf26a
jshstar <jshstar97@gmail.com>
2024-01-09 11:54:59 +0900

    #56 [update-admin] 공연장 추가 기능 Service 업데이트

--- src/main/java/com/sparta/ticketauction/domain/seat/service/SeatServiceImpl.java
+++ src/main/java/com/sparta/ticketauction/domain/seat/service/SeatServiceImpl.java
@@ -1,10 +1,21 @@
 package com.sparta.ticketauction.domain.seat.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.sparta.ticketauction.domain.seat.entity.Seat;
+import com.sparta.ticketauction.domain.seat.repository.SeatRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class SeatServiceImpl implements SeatService {
+
+	private final SeatRepository seatRepository;
+
+	public List<Seat> saveAllSeat(List<Seat> seats) {
+		return seatRepository.saveAll(seats);
+	}
 }
