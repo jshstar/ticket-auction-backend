86c99ce257270ef1be2c768f47921f31c455cbf3
jshstar <jshstar97@gmail.com>
2024-01-10 01:33:11 +0900

    #57 [update-admin] 공연 추가 기능 Controller 구현

--- src/main/java/com/sparta/ticketauction/global/response/SuccessCode.java
+++ src/main/java/com/sparta/ticketauction/global/response/SuccessCode.java
@@ -14,21 +14,8 @@ public enum SuccessCode {
 	SUCCESS_USER_LOGIN(HttpStatus.OK, "U00100", "로그인에 성공했습니다."),
 	SUCCESS_USER_LOGOUT(HttpStatus.OK, "U00200", "로그아웃에 성공했습니다"),
 
-
-
-
-
-
-
 	/* GOODS */
-
-
-
-
-
-
-
-
+	SUCCESS_GOODS_AND_SEQUENCE_CREATE(HttpStatus.CREATED, "G09900", "공연 및 회차가 추가되었습니다."),
 
 	/* PLACE */
 	SUCCESS_PLACE_CREATE(HttpStatus.CREATED, "P09900", "공연장이 추가되었습니다."),
