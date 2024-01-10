3e03bc4dbaaa00c5b1a190f2e35e01e43396b125
MinJung Kim <kminss.dev@gmail.com>
2024-01-10 14:52:19 +0900

    #14 [fix-auction] 입찰하기 기능 - Redisson Lock 설정 수정
    - Redis Lock 보다 Transaction timeout이 늦게 풀려 동시성이 깨지는 문제 수정
      => 락의 leaseTime을 TransactionTimeout보다 늦게 풀리게 time 설정

--- src/main/java/com/sparta/ticketauction/global/annotaion/DistributedLock.java
+++ src/main/java/com/sparta/ticketauction/global/annotaion/DistributedLock.java
@@ -24,11 +24,11 @@ public @interface DistributedLock {
 	 * 락을 기다리는 시간 (default - 5s)
 	 * 락 획득을 위해 waitTime 만큼 대기한다
 	 */
-	long waitTime() default 3L;
+	long waitTime() default 8L;
 
 	/**
 	 * 락 임대 시간 (default - 3s)
 	 * 락을 획득한 이후 leaseTime 이 지나면 락을 해제한다
 	 */
-	long leaseTime() default 2L;
+	long leaseTime() default 6L;
 }
