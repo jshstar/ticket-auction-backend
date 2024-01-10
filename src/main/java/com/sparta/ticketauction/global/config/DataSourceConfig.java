cf81daa804bcd04b5146ef7b3a24ddbb7722c4af
MinJung Kim <kminss.dev@gmail.com>
2024-01-10 14:59:06 +0900

    #93 [init-auction] DB 커넥션 풀 설정 추가
    - 동시 사용자들과 많은 트래픽 처리를 위해 DB의 커넥션 풀 설정이 필요함
      설정에 필요한 구성 추가함

---
+++ src/main/java/com/sparta/ticketauction/global/config/DataSourceConfig.java
@@ -0,0 +1,24 @@
+package com.sparta.ticketauction.global.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.zaxxer.hikari.HikariDataSource;
+@Configuration
+public class DataSourceConfig {
+	@Bean
+	@Primary
+	@ConfigurationProperties("spring.datasource")
+	public DataSourceProperties dataSourceProperties() {
+		return new DataSourceProperties();
+	}
+
+	@Bean
+	@ConfigurationProperties("spring.datasource.hikari")
+	public HikariDataSource dataSource(DataSourceProperties properties) {
+		return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+	}
+}
