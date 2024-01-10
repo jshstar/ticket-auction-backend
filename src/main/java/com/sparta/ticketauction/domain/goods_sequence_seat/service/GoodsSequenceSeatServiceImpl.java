c59bb4c2caa6410390668b13189c31cf5277ac99
jshstar <jshstar97@gmail.com>
2024-01-11 00:16:59 +0900

    #96 [update-admin] GoodsSequenceSeatServiceImpl 쿼리 메서드 기능 업데이트

--- src/main/java/com/sparta/ticketauction/domain/goods_sequence_seat/service/GoodsSequenceSeatServiceImpl.java
+++ src/main/java/com/sparta/ticketauction/domain/goods_sequence_seat/service/GoodsSequenceSeatServiceImpl.java
@@ -1,11 +1,26 @@
 package com.sparta.ticketauction.domain.goods_sequence_seat.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.sparta.ticketauction.domain.goods_sequence_seat.entity.GoodsSequenceSeat;
+import com.sparta.ticketauction.domain.goods_sequence_seat.repository.GoodsSequenceSeatRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class GoodsSequenceSeatServiceImpl implements GoodsSequenceSeatService {
+
+	private final GoodsSequenceSeatRepository goodsSequenceSeatRepository;
+
+	public void saveAllGoodsSequenceSeat(List<GoodsSequenceSeat> goodsSequenceSeatList) {
+		goodsSequenceSeatRepository.saveAll(goodsSequenceSeatList);
+	}
+
+	public List<GoodsSequenceSeat> findAllBySequenceId(Long sequenceId) {
+		return goodsSequenceSeatRepository.findAllBySequenceId(sequenceId);
+	}
 
 }
