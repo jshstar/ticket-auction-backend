972bd73e9875b45f9585b9a839e48ec17391b2bf
Kimhyeyun <cqqwer@gmail.com>
2024-01-10 16:55:44 +0900

    #16 [init - user] 휴대폰 번호 인증을 위한 dto

---
+++ src/main/java/com/sparta/ticketauction/domain/user/request/SmsMessageRequest.java
@@ -0,0 +1,37 @@
+package com.sparta.ticketauction.domain.user.request;
+
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SmsMessageRequest {
+
+	private String type;
+	private String contentType;
+	private String countryCode;
+	private String from;
+	private List<UserForVerificationRequest> messages;
+	private String content;
+
+	@Builder
+	private SmsMessageRequest(
+		String type,
+		String contentType,
+		String countryCode,
+		String from,
+		List<UserForVerificationRequest> messages,
+		String content
+	) {
+		this.type = type;
+		this.contentType = contentType;
+		this.countryCode = countryCode;
+		this.from = from;
+		this.content = content;
+		this.messages = messages;
+	}
+}
