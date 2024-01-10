ecf6f015afa182d4356fdf693462b9e7c588aecc
jshstar <jshstar97@gmail.com>
2024-01-11 00:21:21 +0900

    #96 [update-admin] 공연 회차별 좌석 Controller 업데이트

--- src/main/java/com/sparta/ticketauction/domain/admin/controller/AdminController.java
+++ src/main/java/com/sparta/ticketauction/domain/admin/controller/AdminController.java
@@ -10,11 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.sparta.ticketauction.domain.admin.request.GoodsRequest;
+import com.sparta.ticketauction.domain.admin.request.GoodsSequenceSeatRequest;
 import com.sparta.ticketauction.domain.admin.request.PlaceRequest;
 import com.sparta.ticketauction.domain.admin.response.PlaceResponse;
 import com.sparta.ticketauction.domain.admin.service.AdminServiceImpl;
@@ -33,22 +35,23 @@ public class AdminController {
 
 	private final AdminServiceImpl adminService;
 
-	// 공연장 추가
+	// 공연장
 	@PostMapping("/admin/places")
 	public ResponseEntity<ApiResponse<List<PlaceResponse>>> createPlace(
 		@Valid @RequestBody PlaceRequest placeRequest) {
 		List<PlaceResponse> placeResponse = adminService.createPlace(placeRequest);
 		return ResponseEntity
-			.status(SUCCESS_PLACE_CREATE.getHttpStatus())
+			.status(SUCCESS_PLACE_AND_SEAT_CREATE.getHttpStatus())
 			.body(
 				ApiResponse.of(
-					SUCCESS_PLACE_CREATE.getCode(),
-					SUCCESS_PLACE_CREATE.getMessage(),
+					SUCCESS_PLACE_AND_SEAT_CREATE.getCode(),
+					SUCCESS_PLACE_AND_SEAT_CREATE.getMessage(),
 					placeResponse
 				)
 			);
 	}
 
+	// 공연 및 이미지, 카테고리, 회차
 	@PostMapping(value = "/admin/places/{placeId}/goods",
 		consumes = {
 			MediaType.APPLICATION_JSON_VALUE,
@@ -66,6 +69,24 @@ public class AdminController {
 				ApiResponse.of(
 					SUCCESS_GOODS_AND_SEQUENCE_CREATE.getCode(),
 					SUCCESS_GOODS_AND_SEQUENCE_CREATE.getMessage()
+				)
+			);
+	}
+
+	// 공연별 회차
+	@PostMapping("/admin/goods-sequence-seat")
+	public ResponseEntity<ApiResponse<EmptyObject>> createGoodsSequenceSeatAndAuction(
+		@RequestParam Long placeId,
+		@RequestParam Long sequenceId,
+		@RequestBody GoodsSequenceSeatRequest goodsSequenceSeatRequest) {
+
+		adminService.createGoodsSequenceSeatAndAuction(placeId, sequenceId, goodsSequenceSeatRequest);
+
+		return ResponseEntity
+			.status(SUCCESS_GOODS_SEQUENCE_SEAT_AND_AUCTION_CREATE.getHttpStatus())
+			.body(ApiResponse.of(
+					SUCCESS_GOODS_SEQUENCE_SEAT_AND_AUCTION_CREATE.getCode(),
+					SUCCESS_GOODS_SEQUENCE_SEAT_AND_AUCTION_CREATE.getMessage()
 				)
 			);
 	}
