a1f0fce6a8146702c3120d2e214b3d38479751ab
Kimhyeyun <cqqwer@gmail.com>
2024-01-12 16:07:19 +0900

    #10 [refactor - user] 유저 전화번호 regex 틀릴 시, 메세지 수정

--- src/main/java/com/sparta/ticketauction/domain/user/request/UserPhoneUpdateRequest.java
+++ src/main/java/com/sparta/ticketauction/domain/user/request/UserPhoneUpdateRequest.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 public class UserPhoneUpdateRequest {
 
 	@NotBlank(message = "필수 입력입니다.")
-	@Pattern(regexp = "^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}$", message = "전화번호 형식으로 입력해주세요.")
+	@Pattern(regexp = "^01([0|1|6|7|8|9])?([0-9]{3,4})?([0-9]{4})$", message = "숫자로만 입력해주세요.")
 	private final String phoneNumber;
 
 	@NotBlank(message = "필수 입력입니다.")
