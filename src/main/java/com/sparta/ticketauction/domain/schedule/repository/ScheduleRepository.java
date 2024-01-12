c9e38575b111d1f45d29f32e8632b6eadf5a7509
jshstar <jshstar97@gmail.com>
2024-01-13 01:41:40 +0900

    #135 [fix-place] 주석 처리

--- src/main/java/com/sparta/ticketauction/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/sparta/ticketauction/domain/schedule/repository/ScheduleRepository.java
@@ -1,17 +1,14 @@
 package com.sparta.ticketauction.domain.schedule.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.sparta.ticketauction.domain.schedule.entity.Schedule;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
-	@Query("select s from Schedule s "
-		+ "join fetch s.goods g "
-		+ "join fetch g.goodsImage i "
-		+ "where s.id = :sequenceId and i.type = 'POSTER_IMG'")
-	Optional<Schedule> findScheduleWithGoodsById(Long scheduleId);
+	// @Query("select s from Schedule s "
+	// 	// 	+ "join fetch s.goods g "
+	// 	// 	+ "join fetch g.goodsImage i "
+	// 	// 	+ "where s.id = :sequenceId and i.type = 'POSTER_IMG'")
+	// 	// Optional<Schedule> findScheduleWithGoodsById(Long scheduleId);
 }
