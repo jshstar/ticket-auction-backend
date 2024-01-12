c5a2b5a4d36e29a9854ed3c9c176f8a8ab734698
jshstar <jshstar97@gmail.com>
2024-01-12 23:39:08 +0900

    #135 [update-place] 공연장 컨트롤러 및 응답 코드 업데이트

--- src/main/java/com/sparta/ticketauction/domain/admin/controller/AdminController.java
+++ src/main/java/com/sparta/ticketauction/domain/admin/controller/AdminController.java
@@ -1,10 +1,21 @@
 package com.sparta.ticketauction.domain.admin.controller;
 
+import static com.sparta.ticketauction.global.response.SuccessCode.*;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sparta.ticketauction.domain.admin.request.PlaceRequest;
+import com.sparta.ticketauction.domain.admin.response.PlaceResponse;
 import com.sparta.ticketauction.domain.admin.service.AdminServiceImpl;
+import com.sparta.ticketauction.global.response.ApiResponse;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,5 +26,20 @@ import lombok.extern.slf4j.Slf4j;
 public class AdminController {
 
 	private final AdminServiceImpl adminService;
+
+	@PostMapping("/admin/places")
+	public ResponseEntity<ApiResponse<List<PlaceResponse>>> createPlaceAndZone(
+		@Valid @RequestBody PlaceRequest placeRequest
+	) {
+		List<PlaceResponse> placeResponseList = adminService.createPlaceAndZone(placeRequest);
+		return ResponseEntity
+			.status(SUCCESS_PLACE_AND_ZONE_CREATE.getHttpStatus())
+			.body(
+				ApiResponse.of(
+					SUCCESS_PLACE_AND_ZONE_CREATE.getCode(),
+					SUCCESS_PLACE_AND_ZONE_CREATE.getMessage(),
+					placeResponseList)
+			);
+	}
 
 }
