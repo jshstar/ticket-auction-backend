7985ed4deb3ae8383d0b54b6e6621a6f7a15b5ef
jshstar <jshstar97@gmail.com>
2024-01-14 00:11:48 +0900

    #136 [update-goods] 회차 생성 API 기능 구현

--- src/main/java/com/sparta/ticketauction/domain/schedule/service/ScheduleService.java
+++ src/main/java/com/sparta/ticketauction/domain/schedule/service/ScheduleService.java
@@ -1,17 +1,19 @@
 package com.sparta.ticketauction.domain.schedule.service;
 
+import java.time.LocalTime;
 import java.util.List;
 
+import com.sparta.ticketauction.domain.goods.entity.Goods;
 import com.sparta.ticketauction.domain.schedule.entity.Schedule;
 
 public interface ScheduleService {
-
-	// 회차 리스트 저장
-	void saveAllSchedule(List<Schedule> scheduleList);
-
-	// 회차 저장
-	Schedule saveSchedule(Schedule schedule);
-
+	
 	// 회차 탐색
 	Schedule findSchedule(Long ScheduleId);
+
+	//회차 생성
+	void createSchedule(Goods goods, LocalTime startTime);
+
+	//회차 요일 및 시작시간 부여
+	List<Schedule> distributeSchedule(Goods goods, LocalTime startTime);
 }
