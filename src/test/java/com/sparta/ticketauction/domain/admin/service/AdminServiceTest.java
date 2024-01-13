c57f52bd614f168c7f873a271c282d39f65005df
jshstar <jshstar97@gmail.com>
2024-01-14 00:14:51 +0900

    #136 [test-goods] 공연 생성 API 테스트 코드 추가

--- src/test/java/com/sparta/ticketauction/domain/admin/service/AdminServiceTest.java
+++ src/test/java/com/sparta/ticketauction/domain/admin/service/AdminServiceTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,16 +15,25 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.sparta.ticketauction.domain.admin.request.GoodsRequest;
 import com.sparta.ticketauction.domain.admin.request.PlaceRequest;
 import com.sparta.ticketauction.domain.admin.response.PlaceResponse;
+import com.sparta.ticketauction.domain.goods.entity.Goods;
+import com.sparta.ticketauction.domain.goods.entity.GoodsCategory;
+import com.sparta.ticketauction.domain.goods.entity.GoodsImage;
+import com.sparta.ticketauction.domain.goods.entity.GoodsInfo;
+import com.sparta.ticketauction.domain.goods.service.GoodsInfoServiceImpl;
+import com.sparta.ticketauction.domain.goods.service.GoodsServiceImpl;
 import com.sparta.ticketauction.domain.place.dto.ZoneInfo;
 import com.sparta.ticketauction.domain.place.entity.Place;
 import com.sparta.ticketauction.domain.place.entity.Zone;
 import com.sparta.ticketauction.domain.place.service.PlaceServiceImpl;
 import com.sparta.ticketauction.domain.place.service.ZoneServiceImpl;
+import com.sparta.ticketauction.domain.schedule.entity.Schedule;
+import com.sparta.ticketauction.domain.schedule.service.ScheduleServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
 public class AdminServiceTest {
@@ -35,6 +46,15 @@ public class AdminServiceTest {
 
 	@Mock
 	ZoneServiceImpl zoneService;
+
+	@Mock
+	GoodsInfoServiceImpl goodsInfoService;
+
+	@Mock
+	GoodsServiceImpl goodsService;
+
+	@Mock
+	ScheduleServiceImpl scheduleService;
 
 	public static PlaceRequest placeRequest;
 
@@ -95,6 +115,81 @@ public class AdminServiceTest {
 		assertEquals(response.get(0).getZoneCountSeat(), zoneList.get(0).getSeatNumber());
 		assertEquals(response.get(1).getZone(), zoneList.get(1).getName());
 		assertEquals(response.get(1).getZoneCountSeat(), zoneList.get(1).getSeatNumber());
+
+	}
+
+	@Test
+	void 공연_공연정보_공연이미지_공연카테고리_회차_생성_테스트() {
+		// given
+		Place place = Mockito.mock();
+		GoodsInfo goodsInfo = goodsRequest.toEntity();
+		Goods goods = goodsRequest.toEntity(place, goodsInfo);
+
+		List<String> fileUrl = new ArrayList<>();
+		fileUrl.add("goods/thumbnail/1/51579925-f563-4c75-9999-e2264dadbdab");
+		fileUrl.add("goods/general/1/0aebcd4f-b2b5-4bbc-b8f8-a10c4b8f2c17");
+
+		List<GoodsImage> goodsImage = new ArrayList<>();
+		goodsImage.add(GoodsImage.builder().s3Key(fileUrl.get(0)).type("대표").goodsInfo(goodsInfo).build());
+		goodsImage.add(GoodsImage.builder().s3Key(fileUrl.get(1)).type("일반").goodsInfo(goodsInfo).build());
+
+		goodsInfo.addGoodsImage(goodsImage);
+		GoodsCategory goodsCategory = GoodsCategory.builder().name("공연").build();
+		goodsInfo.updateGoodsCategory(goodsCategory);
+
+		List<Schedule> scheduleList = new ArrayList<>();
+		scheduleList.add(
+			Schedule
+				.builder()
+				.sequence(1)
+				.startDateTime(
+					LocalDateTime.of(2023, 3, 1, 15, 0, 0))
+				.goods(goods)
+				.build()
+		);
+		scheduleList.add(
+			Schedule
+				.builder()
+				.sequence(2)
+				.startDateTime(
+					LocalDateTime.of(2023, 3, 2, 15, 0, 0))
+				.goods(goods)
+				.build()
+		);
+		LocalDateTime startDateTime = scheduleList.get(0).getStartDateTime();
+		LocalDateTime endDateTime = scheduleList.get(1).getStartDateTime();
+		long daysBetween = ChronoUnit.DAYS.between(startDateTime, endDateTime);
+
+		// when
+		given(placeService.getReferenceById(1L)).willReturn(place);
+		given(goodsInfoService.createGoodsInfo(any(GoodsRequest.class))).willReturn(goodsInfo);
+		given(goodsInfoService.createGoodsImage(any(), any())).willReturn(goodsImage);
+		given(goodsInfoService.createGoodsCategory(any())).willReturn(goodsCategory);
+		given(goodsService.createGoods(goodsRequest, place, goodsInfo)).willReturn(goods);
+		adminService.createGoodsBundleAndSchedule(1L, goodsRequest, mock());
+
+		// then
+		verify(placeService, times(1)).getReferenceById(anyLong());
+		verify(goodsInfoService, times(1)).createGoodsInfo(any(GoodsRequest.class));
+		verify(goodsInfoService, times(1)).createGoodsImage(any(), any(GoodsInfo.class));
+		verify(goodsInfoService, times(1)).createGoodsCategory(any());
+		verify(goodsService, times(1))
+			.createGoods(
+				any(GoodsRequest.class),
+				any(Place.class),
+				any(GoodsInfo.class)
+			);
+		verify(scheduleService, times(1)).createSchedule(any(Goods.class), any(LocalTime.class));
+
+		assertEquals(goodsImage.get(0).getS3Key(), goodsInfo.getGoodsImage().get(0).getS3Key());
+		assertEquals(goodsImage.get(1).getS3Key(), goodsInfo.getGoodsImage().get(1).getS3Key());
+		assertEquals(goodsImage.get(0).getType(), goodsInfo.getGoodsImage().get(0).getType());
+		assertEquals(goodsImage.get(1).getType(), goodsInfo.getGoodsImage().get(1).getType());
+		assertEquals(goodsCategory.getName(), goodsInfo.getGoodsCategory().getName());
+		assertEquals((int)daysBetween, scheduleList.get(1).getSequence());
+		assertEquals(goodsRequest.getStartTime().getMinute(), scheduleList.get(1).getStartDateTime().getMinute());
+		assertEquals(goodsRequest.getStartTime().getHour(), scheduleList.get(1).getStartDateTime().getHour());
+		assertEquals(scheduleList.get(1).getGoods().getGoodsInfo(), goodsInfo);
 
 	}
 }
