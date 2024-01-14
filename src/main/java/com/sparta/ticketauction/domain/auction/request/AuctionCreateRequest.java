7ab549465dcd41bbed010fd8a9a70fbd6a4af62e
MinJung Kim <kminss.dev@gmail.com>
2024-01-15 05:38:05 +0900

    #125 [refactor-auction] 경매 생성 request 수정
    - 도메인 수정으로 request도 수정하게 됨
    - request 검증 테스트 추가

--- src/main/java/com/sparta/ticketauction/domain/auction/request/AuctionCreateRequest.java
+++ src/main/java/com/sparta/ticketauction/domain/auction/request/AuctionCreateRequest.java
@@ -1,11 +1,12 @@
 package com.sparta.ticketauction.domain.auction.request;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.sparta.ticketauction.domain.auction.entity.Auction;
+import com.sparta.ticketauction.domain.grade.entity.ZoneGrade;
+import com.sparta.ticketauction.domain.schedule.entity.Schedule;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -14,22 +15,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuctionCreateRequest {
 
-	@NotNull(message = "시작가를 입력해주세요.")
-	private final Long startPrice;
+	@NotNull(message = "경매 좌석 번호를 입력해주세요.")
+	private final Integer seatNumber;
 
-	@NotBlank(message = "경매 시작일시를 입력해주세요.")
-	@JsonFormat(pattern = "yyyy-MM-dd HH")
-	private final LocalDateTime startDateTime;
+	// TODO: 1/12/24 경매시작일시, 마감일시 포맷 생각하기 
+	public Auction toEntity(Schedule schedule, ZoneGrade zoneGrade) {
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH");
+		String now = LocalDateTime.now().format(format);
 
-	@NotBlank(message = "경매 마감일시를 입력해주세요.")
-	@JsonFormat(pattern = "yyyy-MM-dd HH")
-	private final LocalDateTime endDateTime;
-
-	public Auction toEntity() {
 		return Auction.builder()
-			.startPrice(startPrice)
-			.startDateTime(startDateTime)
-			.endDateTime(endDateTime)
+			.schedule(schedule)
+			.zoneGrade(zoneGrade)
+			.seatNumber(seatNumber)
+			.startPrice(zoneGrade.getGrade().getAuctionPrice())
+			.bidPrice(zoneGrade.getGrade().getAuctionPrice())
+			.startDateTime(LocalDateTime.parse(now, format))
+			.endDateTime(schedule.getStartDateTime().minusDays(3))
 			.build();
 	}
 }
