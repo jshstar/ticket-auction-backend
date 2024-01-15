30dd5b739d267a19cd3560cfaff43ff77580b944
jshstar <jshstar97@gmail.com>
2024-01-15 20:03:34 +0900

    #159 [fix-global] Admin 응답코드 변경

--- src/main/java/com/sparta/ticketauction/global/response/SuccessCode.java
+++ src/main/java/com/sparta/ticketauction/global/response/SuccessCode.java
@@ -27,6 +27,9 @@ public enum SuccessCode {
 	/* PLACE */
 	SUCCESS_GET_ALL_PLACE(HttpStatus.OK, "P00000", "공연장 전체 조회 성공"),
 
+	/* ZONE */
+
+
 	/* AUCTION */
 	SUCCESS_BID(HttpStatus.CREATED, "A00100", "입찰에 성공했습니다."),
 
@@ -48,11 +51,11 @@ public enum SuccessCode {
 
 
 	/* ADMIN */
-	SUCCESS_GOODS_AND_SCHEDULE_CREATE(HttpStatus.CREATED, "Z09900", "공연정보, 공연 및 회차 생성을 성공했습니다."),
-	SUCCESS_PLACE_AND_ZONE_CREATE(HttpStatus.CREATED, "Z09901", "공연장 및 구역 생성을 성공했습니다."),
-	SUCCESS_GRADE_CREATE(HttpStatus.CREATED, "Z09902", "등급 생성 성공"),
-	SUCCESS_ZONE_GRADE_CREATE(HttpStatus.CREATED, "Z09903", "구역 등급 생성 성공"),
-	SUCCESS_AUCTION_CREATE(HttpStatus.CREATED, "Z09904", "경매 생성 성공"),
+	SUCCESS_GOODS_AND_SCHEDULE_CREATE(HttpStatus.CREATED, "M09900", "공연정보, 공연 및 회차 생성을 성공했습니다."),
+	SUCCESS_PLACE_AND_ZONE_CREATE(HttpStatus.CREATED, "M09901", "공연장 및 구역 생성을 성공했습니다."),
+	SUCCESS_GRADE_CREATE(HttpStatus.CREATED, "M09902", "등급 생성 성공"),
+	SUCCESS_ZONE_GRADE_CREATE(HttpStatus.CREATED, "M09903", "구역 등급 생성 성공"),
+	SUCCESS_AUCTION_CREATE(HttpStatus.CREATED, "M09904", "경매 생성 성공"),
 
 	/* RESERVATION */
 	SUCCESS_RESERVE(HttpStatus.CREATED, "R00000", "예매 성공했습니다."),
