ff37dda3b04baf056bcbb4f466c1c0d3b232c7e0
jshstar <jshstar97@gmail.com>
2024-01-16 02:49:12 +0900

    #17 [update-goods] 공연 페이징 조회 API GoodsInfoRepository 페이징 쿼리 메소드 구현

--- src/main/java/com/sparta/ticketauction/domain/goods/repository/GoodsInfoRepository.java
+++ src/main/java/com/sparta/ticketauction/domain/goods/repository/GoodsInfoRepository.java
@@ -1,8 +1,15 @@
 package com.sparta.ticketauction.domain.goods.repository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.sparta.ticketauction.domain.goods.entity.GoodsInfo;
 
 public interface GoodsInfoRepository extends JpaRepository<GoodsInfo, Long> {
+	@Query("select g from GoodsInfo g where g.goodsCategory.name = :categoryName")
+	Slice<GoodsInfo> findByCategoryName(Pageable pageable, @Param("categoryName") String categoryName);
+
 }
