ef346c0aa808be5155df187ef9ee349f33b07622
Kimhyeyun <cqqwer@gmail.com>
2024-01-17 01:33:41 +0900

    #175 [fix] 쿠키에서 refreshtoken 찾을 때 이름으로 찾기

--- src/main/java/com/sparta/ticketauction/global/jwt/JwtUtil.java
+++ src/main/java/com/sparta/ticketauction/global/jwt/JwtUtil.java
@@ -130,11 +130,14 @@ public class JwtUtil {
 			return null;
 		}
 
-		String refreshToken = "";
+		String refreshToken = null;
 		for (Cookie cookie : cookies) {
-			refreshToken = URLDecoder.decode(cookie.getValue(), StandardCharsets.UTF_8);
+			if (cookie.getName().equals(REFRESH_TOKEN_HEADER)) {
+				refreshToken = URLDecoder.decode(cookie.getValue(), StandardCharsets.UTF_8);
+				break;
+			}
 		}
-		return substringToken(refreshToken);
+		return refreshToken;
 	}
 
 	public String getAccessTokenFromRequestHeader(HttpServletRequest request) {
