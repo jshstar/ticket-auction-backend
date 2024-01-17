43fdf1c75e6945617a65f4a5e9ecebc27703c507
jshstar <jshstar97@gmail.com>
2024-01-18 01:28:13 +0900

    #188 [style-goods] 다음 페이지 버튼 수정

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -158,16 +158,11 @@ function submitGoodsInfo(token) {
         success: function (response) {
             alert('공연 정보가 성공적으로 추가되었습니다.');
             console.log(response);
-            // 폼 초기화 등 추가 작업 수행
-            $("#submitButton").prop('disabled', true).text('제출 됨');
+            movePageWithToken("/goods.html");
         },
         error: function (xhr, status, error) {
             alert('공연 정보를 추가하는 중 오류가 발생했습니다: ' + error);
         },
-        // complete: function () {
-        //     // 요청이 완료되면 버튼을 다시 활성화
-        //     $("#submitButton").prop('disabled', false).text('Add Performance Info');
-        // }
     });
 }
 
