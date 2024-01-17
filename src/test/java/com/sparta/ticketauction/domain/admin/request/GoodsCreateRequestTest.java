584a2325f6633a86959b213546cd2f90ab9c7a24
jshstar <jshstar97@gmail.com>
2024-01-17 15:48:32 +0900

    #176 [test-admin] test 업데이트

--- src/test/java/com/sparta/ticketauction/domain/admin/request/GoodsCreateRequestTest.java
+++ src/test/java/com/sparta/ticketauction/domain/admin/request/GoodsCreateRequestTest.java
@@ -29,6 +29,8 @@ public class GoodsCreateRequestTest {
 	private static ValidatorFactory factory;
 	private static Validator validator;
 
+	String title;
+
 	LocalDate startDate;
 
 	LocalDate endDate;
@@ -44,15 +46,63 @@ public class GoodsCreateRequestTest {
 
 	@BeforeEach
 	void createInit() {
+		this.title = "레미제라블-서울";
 		this.startDate = LocalDate.of(2023, 3, 1);
 		this.endDate = LocalDate.of(2023, 4, 1);
 		this.startTime = LocalTime.of(12, 30);
 	}
 
 	@Test
+	void 공연_타이틀_빈공간_검증_테스트() {
+		// given
+		GoodsCreateRequest goodsCreateRequest = new GoodsCreateRequest(
+			null,
+			this.startDate,
+			this.endDate,
+			this.startTime
+		);
+
+		//when
+		Set<ConstraintViolation<GoodsCreateRequest>> violations = validator.validate(goodsCreateRequest);
+
+		//then
+		assertFalse(violations.isEmpty());
+		violations
+			.forEach(
+				error -> {
+					assertThat(error.getMessage()).isEqualTo("공연제목 기입은 필수 입니다.");
+				});
+
+	}
+
+	@Test
+	void 공연_타이틀_패턴_검증_테스트() {
+		// given
+		GoodsCreateRequest goodsCreateRequest = new GoodsCreateRequest(
+			"레미제라블서울",
+			this.startDate,
+			this.endDate,
+			this.startTime
+		);
+
+		//when
+		Set<ConstraintViolation<GoodsCreateRequest>> violations = validator.validate(goodsCreateRequest);
+
+		//then
+		assertFalse(violations.isEmpty());
+		violations
+			.forEach(
+				error -> {
+					assertThat(error.getMessage()).isEqualTo("입력 양식: (공연제목)-(지역 및 목표) 입니다.");
+				});
+
+	}
+
+	@Test
 	void 공연_시작날짜_검증_테스트() {
 		// given
 		GoodsCreateRequest goodsCreateRequest = new GoodsCreateRequest(
+			this.title,
 			null,
 			this.endDate,
 			this.startTime
@@ -75,6 +125,7 @@ public class GoodsCreateRequestTest {
 	void 공연_종료날짜_검증_테스트() {
 		// given
 		GoodsCreateRequest goodsCreateRequest = new GoodsCreateRequest(
+			this.title,
 			this.startDate,
 			null,
 			this.startTime
@@ -97,6 +148,7 @@ public class GoodsCreateRequestTest {
 	void 공연_상연시간_검증_테스트() {
 		// given
 		GoodsCreateRequest goodsCreateRequest = new GoodsCreateRequest(
+			this.title,
 			this.startDate,
 			this.endDate,
 			null
@@ -119,6 +171,7 @@ public class GoodsCreateRequestTest {
 	void 공연_Entity_생성_성공_테스트() {
 		// given
 		GoodsCreateRequest goodsCreateRequest = new GoodsCreateRequest(
+			this.title,
 			this.startDate,
 			this.endDate,
 			this.startTime
@@ -130,6 +183,7 @@ public class GoodsCreateRequestTest {
 		Goods goods = goodsCreateRequest.toEntity(place, goodsInfo);
 
 		//then
+		assertEquals(this.title, goods.getTitle());
 		assertEquals(this.startDate.getYear(), goods.getStartDate().getYear());
 		assertEquals(this.startDate.getMonth(), goods.getStartDate().getMonth());
 		assertEquals(this.startDate.getDayOfMonth(), goods.getStartDate().getDayOfMonth());
