5ee515f9cca91890e2c235efaec0d0313abe05c8
Kimhyeyun <cqqwer@gmail.com>
2024-01-17 23:48:56 +0900

    #185 [update] 유저 정보 수정 프론트 구현

--- src/main/resources/static/js/mypage-menu.js
+++ src/main/resources/static/js/mypage-menu.js
@@ -7,7 +7,6 @@ function getMyInfo(token, id) {
         },
         success: function (data) {
             let info = data.data;
-            console.log(info.email)
             $("#info-email").text(info.email);
             $("#info-name").text(info.name);
             $("#info-nickname").text(info.nickname);
@@ -19,4 +18,78 @@ function getMyInfo(token, id) {
             console.error(textStatus);
         },
     })
+}
+
+function verificationPhone() {
+    let phoneNumber = $("#update-phone").val();
+    $.ajax({
+        type: "POST",
+        url: `/api/v1/auth/sms`,
+        contentType: "application/json",
+        data: JSON.stringify({
+            to: phoneNumber
+        }),
+        success: function (data) {
+            alert("인증 번호를 발송했습니다.");
+        },
+        error: function (jqXHR, textStatus) {
+
+            let response = jqXHR.responseJSON;
+            if (response.data) {
+                let data = response.data;
+                if (data.to) {
+                    $("#update-phoneNumber-span").text(data.to);
+                }
+            }
+            if (response.message) {
+                if (response.code.substring(2, 4) === "07") {
+                    $("#update-verificationNumber-span").text(response.message);
+                }
+            }
+        },
+    });
+}
+
+function updateUserInfo(token, id) {
+    let nickname = $("#update-nickname").val();
+    let phoneNumber = $("#update-phone").val();
+    let verificationNumber = $("#update-verificationCode").val();
+
+    $.ajax({
+        type: "PUT",
+        url: `/api/v1/users/${id}`,
+        contentType: "application/json",
+        headers: {
+            "Authorization": token
+        },
+        data: JSON.stringify({
+            nickname: nickname,
+            phoneNumber: phoneNumber,
+            verificationNumber: verificationNumber
+        }),
+        success: function (data) {
+            alert("회원 정보를 수정했습니다.")
+            movePageWithToken(`/user-info.html`);
+        },
+        error: function (jqXHR, textStatus) {
+            resetValidationMessages();
+            let response = jqXHR.responseJSON;
+            if (response) {
+                if (response.code.substring(2, 4) === "04") {
+                    $("#update-nickname-span").text(response.message);
+                }
+                if (response.code.substring(2, 4) === "06") {
+                    $("#update-phoneNumber-span").text(response.message);
+                }
+                if (response.code.substring(2, 4) === "07") {
+                    $("#update-verificationNumber-span").text(response.message);
+                }
+            }
+        }
+    });
+}
+
+function resetValidationMessages() {
+    $("#update-nickname-span, #update-phoneNumber-span, #update-verificationNumber-span")
+        .text("");
 }
