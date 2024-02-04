59732e12945fddc335f8d2e5d9f49d3ca152ea95
OuOHoon <begreat@kakao.com>
2024-02-04 23:18:40 +0900

    # [fix-reservation] 좌석 상태 캐시 워밍업을 한 인스턴스만 실행하도록 간단한 락 추가

--- src/main/java/com/sparta/ticketauction/domain/reservation/reservation_seat/service/ReservationSeatServiceImpl.java
+++ src/main/java/com/sparta/ticketauction/domain/reservation/reservation_seat/service/ReservationSeatServiceImpl.java
@@ -41,6 +41,14 @@ public class ReservationSeatServiceImpl implements ReservationSeatService {
 	@Transactional(readOnly = true)
 	public void seatCacheWarmUp() {
 
+		// 워밍업 작업은 한 인스턴스만 실행하도록 한다.
+		String lockKey = "SEAT_WARM_UP_LOCK";
+		String lockValue = "locked";
+		Boolean acquired = redisTemplate.opsForValue().setIfAbsent(lockKey, lockValue, 15, TimeUnit.MINUTES);
+		if (Boolean.FALSE.equals(acquired)) {
+			return;
+		}
+
 		int pageNumber = 0;
 		int pageSize = 50000; // 페이지 당 요소 수
 		Pageable pageable = PageRequest.of(pageNumber, pageSize);
